0d8728832e716f409aadd4acaaf87b54085d5708
mashago <mashacoder@foxmail.com>
2019-07-14 08:59:17 +0800

    first commit

---
+++ cerberus-src/cerberus_core.h
@@ -0,0 +1,4 @@
+
+#pragma once
+
+void cerberus_start();
---
+++ cerberus-src/cerberus_main.cpp
@@ -0,0 +1,15 @@
+
+extern "C"
+{
+#include <stdio.h>
+}
+#include "cerberus_core.h"
+
+int main(int argc, char** argv)
+{
+	printf("hello cerberus2\n");
+
+	cerberus_start();
+
+	return 0;
+}
