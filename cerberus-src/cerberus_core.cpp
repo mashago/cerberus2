eafe65f8207111389f31d6324be8b5e0d7a1b20d
mashago <mashacoder@foxmail.com>
2019-07-15 07:42:17 +0800

    update core

--- cerberus-src/cerberus_core.cpp
+++ cerberus-src/cerberus_core.cpp
@@ -1,4 +1,10 @@
 
+extern "C"
+{
+#include <stdio.h>
+#include <unistd.h>
+}
+#include <thread>
 #include "cerberus_core.h"
 
 int cerberus_create_monopoly_thread_service()
@@ -13,8 +19,26 @@ int cerberus_create_share_thread_service()
 	return 0;
 }
 
+void share_thread_run(int id)
+{
+	printf("share_thread_run=%d\n", id);
+}
+
 void cerberus_start()
 {
 	// TODO init share work threads
 
+	int thread_count = 3;
+
+	std::thread tl[thread_count];
+	for (int i = 0; i < thread_count; ++i)
+	{
+		printf("i=%d\n", i);
+		tl[i] = std::thread(share_thread_run, i);
+	}
+	
+	for (int i = 0; i < thread_count; ++i)
+	{
+		tl[i].join();
+	}
 }
