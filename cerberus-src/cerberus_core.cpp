96dc1283caffe145473c7a1a242e2f1a2dea5d8b
mashago <mashacoder@foxmail.com>
2019-07-16 07:45:50 +0800

    update core

--- cerberus-src/cerberus_core.cpp
+++ cerberus-src/cerberus_core.cpp
@@ -7,13 +7,13 @@ extern "C"
 #include <thread>
 #include "cerberus_core.h"
 
-int cerberus_create_monopoly_thread_service()
+int Cerberus::cerberus_create_monopoly_thread_service()
 {
 	// TODO create thread, create service, push into service list, push start event
 	return 0;
 }
 
-int cerberus_create_share_thread_service()
+int Cerberus::cerberus_create_share_thread_service()
 {
 	// TODO create service, push into service list, push start event
 	return 0;
--- cerberus-src/cerberus_core.h
+++ cerberus-src/cerberus_core.h
@@ -1,4 +1,27 @@
 
 #pragma once
 
+#include <list>
+#include <map>
+#include <mutex>
+#include <condition_variable>
+
 void cerberus_start();
+
+struct CerberusEvent {
+};
+
+struct CerberusService {
+	int id;
+	std::list<CerberusEvent> event_list;
+};
+
+class Cerberus {
+public:
+	int cerberus_create_monopoly_thread_service();
+	int cerberus_create_share_thread_service();
+private:
+	std::mutex mt;
+	std::condition_variable cv;
+	std::map<int, CerberusService*> service_map;
+};
