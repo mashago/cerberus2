71cff102fc54be82191aec51a0d4d8f294f25cb9
masha <mashacoder@foxmail.com>
2019-07-22 15:53:50 +0800

    update event logic

--- cerberus-src/cerberus_core.cpp
+++ cerberus-src/cerberus_core.cpp
@@ -13,9 +13,22 @@ int Cerberus::create_monopoly_thread_service()
 	return 0;
 }
 
+static int gen_service_id()
+{
+	static int id = 0;
+	return ++id;
+}
+
 int Cerberus::create_share_thread_service()
 {
 	// TODO create service, push into service list, push start event
+	CerberusService* service = new CerberusService();
+	service->id = gen_service_id();
+	service_map.insert(std::make_pair(service->id, service));
+	CerberusEvent* start_event = new CerberusEvent();
+	start_event->type = 1;
+	push_event(service, start_event);
+
 	return 0;
 }
 
@@ -33,8 +46,14 @@ CerberusService* Cerberus::get_active_service()
 
 void Cerberus::push_event(CerberusService* service, CerberusEvent* event)
 {
-	std::unique_lock<std::mutex> lock(service->mtx);
+	std::unique_lock<std::mutex> lock_big(active_service_mtx);
+	std::unique_lock<std::mutex> lock_small(service->mtx);
 	service->event_list.push_back(event);
+	if (!service->is_active)
+	{
+		service->is_active = true;
+		active_service_list.push_back(service);
+	}
 	active_service_cv.notify_all();
 }
 
@@ -53,11 +72,37 @@ CerberusEvent* Cerberus::pop_event(CerberusService* service)
 bool Cerberus::handle_event()
 {
 	CerberusService* service = get_active_service();
-	if (service == nullptr)
+	if (!service)
 	{
 		return false;
 	}
+
+	CerberusEvent* event = pop_event(service);
+	if (!event)
+	{
+		after_handle_event(service);
+		return false;
+	}
+
+	// TODO handle event
+
+	after_handle_event(service);
 	return true;
+}
+
+void Cerberus::after_handle_event(CerberusService* service)
+{
+	std::unique_lock<std::mutex> lock_big(active_service_mtx);
+	std::unique_lock<std::mutex> lock_small(service->mtx);
+	if (service->event_list.empty())
+	{
+		service->is_active = false;
+	}
+	else
+	{
+		service->is_active = true;
+		active_service_list.push_back(service);
+	}
 }
 
 static std::mutex thread_mtx;
--- cerberus-src/cerberus_core.h
+++ cerberus-src/cerberus_core.h
@@ -8,16 +8,21 @@
 
 void cerberus_start();
 
-struct CerberusEvent {
+struct CerberusEvent
+{
+	int type;
 };
 
-struct CerberusService {
+struct CerberusService
+{
 	int id;
+	bool is_active;
 	std::mutex mtx;
 	std::list<CerberusEvent*> event_list;
 };
 
-class Cerberus {
+class Cerberus
+{
 public:
 	std::list<CerberusService*> active_service_list;
 	int create_monopoly_thread_service();
@@ -33,4 +38,5 @@ private:
 	std::map<int, CerberusService*> service_map;
 
 	CerberusService* get_active_service();
+	void after_handle_event(CerberusService* service);
 };
